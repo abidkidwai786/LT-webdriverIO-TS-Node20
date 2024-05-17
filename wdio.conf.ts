135835f0e013e5406bcaaa1254e2f74cc03e5ec3
Abid Kidwai <abidkidwai786@gmail.com>
2024-05-18 02:00:33 +0530

    first commit1

---
+++ wdio.conf.ts
@@ -0,0 +1,57 @@
+import type { Options } from '@wdio/types';
+
+
+
+export const config: Options.Testrunner = {
+
+    runner: 'local',
+    autoCompileOpts: {
+        autoCompile: true,
+        tsNodeOpts: {
+            project: './test/tsconfig.json',
+            transpileOnly: true
+        }
+    },
+    hostname: 'hub.lambdatest.com',
+    port: 80,
+    path: '',
+    user: process.env.LT_USERNAME,
+    key: process.env.LT_ACCESS_KEY,
+    specs: [
+        './test/specs/**/*.ts'
+    ], 
+    exclude: [
+        // 'path/to/excluded/files'
+    ],
+    maxInstances: 10,
+
+
+
+    capabilities: [
+        {
+            browserName: "Chrome",
+            browserVersion: "122.0",
+            path: "/wd/hub",
+            hostname: "hub.lambdatest.com",
+            port: 80,
+            "LT:Options": {
+                "username": "",
+                "accessKey": "",               
+                "platformName": "Windows 10",
+            }
+        },
+    ],
+
+    logLevel: 'info',
+    bail: 0,
+    baseUrl: 'http://localhost',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    framework: 'mocha',
+    reporters: ['spec'],
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    },
+}
